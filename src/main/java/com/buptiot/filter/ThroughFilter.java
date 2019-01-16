ac2ec40e65c3e1c51d18b2893e505772eaabbe5c
BJJ-zhangyufei <1535256336@qq.com>
2019-01-16 16:05:44 +0800

    增加ThrouguFilter配置

---
+++ src/main/java/com/buptiot/filter/ThroughFilter.java
@@ -0,0 +1,33 @@
+package com.buptiot.filter;
+
+/**
+ * Created by zyf on 2019/1/16.
+ */
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Configuration
+public class ThroughFilter implements Filter{
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "*");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+}
+
